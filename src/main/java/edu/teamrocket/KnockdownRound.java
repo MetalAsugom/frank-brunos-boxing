390872b3df84e2dd6b6cf52cc4c929a041aa27e3
Pablo González <pablogonzaleztemes@gmail.com>
2026-04-20 19:53:01 +0200

    refactor(KnockdownRound): :recycle: refactor class code

--- src/main/java/edu/teamrocket/KnockdownRound.java
+++ src/main/java/edu/teamrocket/KnockdownRound.java
@@ -3,11 +3,12 @@ package edu.teamrocket;
 public class KnockdownRound implements Round{
 
     private String round;
-    private byte redBoxerScore;
     private byte blueBoxerScore;
+    private byte redBoxerScore;
 
-    public KnockdownRound (String round) {
+    public KnockdownRound(String round) {
         this.round = round;
+        boxerRoundScore();
     }
 
     @Override
@@ -22,11 +23,16 @@ public class KnockdownRound implements Round{
 
     @Override
     public void boxerRoundScore() {
-        this.redBoxerScore = Byte.parseByte(round.split("-")[1]);
-        this.blueBoxerScore = Byte.parseByte(round.split("-")[0]);
+        this.blueBoxerScore = Byte.parseByte(round.split("-")[1].replace(" ", ""));
+        this.redBoxerScore = Byte.parseByte(round.split("-")[0].replace(" ", ""));
     }
 
     public String getRoundScore() {
-        return round.split("-")[1] + "-" + round.split("-")[0];
+        return this.toString();
+    }
+
+    @Override
+    public String toString() {
+        return this.redBoxerScore + "-" + this.blueBoxerScore;
     }
 }
