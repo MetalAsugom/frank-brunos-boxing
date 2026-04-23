c51ad4e30daba3fef033c7a4f7ce818d89496333
Pablo Gonzalez <pablogonzaleztemes@gmail.com>
2026-04-23 09:39:39 +0200

    refactor(RoundFactory): :recycle: refactor switch with modern syntax

--- src/main/java/edu/teamrocket/RoundFactory.java
+++ src/main/java/edu/teamrocket/RoundFactory.java
@@ -5,22 +5,11 @@ public class RoundFactory {
     private RoundFactory() {}
 
     public static Round getRound(String rounds) {
-
-        switch (rounds) {
-            case "10 - 9":
-            case "9 - 10":
-                return new RegularRound(rounds);
-        
-            case "10 - 8":
-            case "8 - 10":
-                return new KnockdownRound(rounds);
-            
-            case "1, 8 - 10":
-            case "10 - 8 ,1":
-                return new PointsDeducted(rounds);
-            
-            default:
-                return null;
-        }
+        return switch (rounds) {
+            case "10 - 9", "9 - 10" -> new RegularRound(rounds);
+            case "10 - 8", "8 - 10" -> new KnockdownRound(rounds);
+            case "1, 8 - 10", "10 - 8 ,1" -> new PointsDeducted(rounds);
+            default -> null; 
+        };
     }
 }
